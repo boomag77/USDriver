130a5846525294f36a86765e488605359f90c9e3
boomag77 <96821826+boomag77@users.noreply.github.com>
2022-08-17 00:53:20 +0300

    Initial commit

--- USDriver/Model/Test.swift
+++ USDriver/Model/Test.swift
@@ -7,7 +7,13 @@
 
 import Foundation
 
+enum Variant {
+    case test
+    case quiz
+}
+
 protocol TestProtocol {
+    var variant: Variant { get set }
     var numberOfCards: Int { get set }
     var cards: [Card] { get set }
     func isRight(answer: QuestionProtocol) -> Bool
